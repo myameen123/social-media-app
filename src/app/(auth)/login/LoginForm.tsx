26ab0f0931e61ebe2197027bcd9a703c205ae29b
myameen123 <yameenmuhammad960@gmail.com>
2024-08-15 11:27:08 +0500

    add authentication

---
+++ src/app/(auth)/login/LoginForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { loginSchema, LoginValues } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState, useTransition } from "react";
+// import { signUp } from "./actions";
+import { PasswordInput } from "@/components/PasswordInput";
+import LoadingButton from "@/components/LoadingButton";
+import { login } from "./actions";
+
+function LoginForm() {
+  const [error, setError] = useState<string>();
+  const [isPending, startTransition] = useTransition();
+  const form = useForm<LoginValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  async function onSubmit(values: LoginValues) {
+    setError(undefined);
+    startTransition(async () => {
+      const { error } = await login(values);
+      if (error) setError(error);
+    });
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        {error && <p className="text-center text-destructive">{error}</p>}
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <LoadingButton loading={isPending} type="submit" className="w-full">
+          Create account
+        </LoadingButton>
+      </form>
+    </Form>
+  );
+}
+
+export default LoginForm;
